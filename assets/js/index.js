e9a535b13a3a131db22a2535c6536048e4cad906
Eternal <vladultimate@gmail.com>
2021-10-08 13:59:53 +0300

    $

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,16 +3,10 @@ let board = document.querySelector('.container');
 const boxes = document.querySelector('.boxes');
 const addDays = document.querySelectorAll('.days')
 const datePage = document.querySelector('.date')
-<<<<<<< HEAD
 const date = new Date()
 const fullDay = date.getDate()  + '.' + date.getMonth() + '.' + date.getFullYear();
 const day = date.getDate()
 /*const obj = {
-=======
-let date = new Date()
-let day = date.getDay()
-const obj = {
->>>>>>> fa93ebdfe71bfb6059c08e68ce9f7d51a14ff702
     January: 31,
     February: 28,
     March: 31,
@@ -27,12 +21,8 @@ const obj = {
     December: 31,
 
    
-<<<<<<< HEAD
 }*/
 const month = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
-=======
-}
->>>>>>> fa93ebdfe71bfb6059c08e68ce9f7d51a14ff702
 const NUMBER = 35;
 const allDay = 31;
 const arrDays = ['S', 'F', 'T', 'W', 'T', 'M', 'S']
@@ -40,7 +30,6 @@ window.onload = onLo;
 
 
 function onLo(){
-<<<<<<< HEAD
     //datePage.innerHTML = day
     const numMonth = date.getMonth()
     let nameMonth = month[numMonth]
@@ -51,10 +40,6 @@ function onLo(){
    
 
     
-=======
-    //const addDays = document.querySelectorAll('.days')
-    datePage.innerHTML = day
->>>>>>> fa93ebdfe71bfb6059c08e68ce9f7d51a14ff702
 //Создание Дней Недели
 for(let f = 0; f<=6;f++){
     let day = arrDays[f]
@@ -82,12 +67,6 @@ for(let z = 0;z<=30;z++)
             box[z-1].classList.add('active')
         }
     }
-<<<<<<< HEAD
-=======
-
-    
-}
->>>>>>> fa93ebdfe71bfb6059c08e68ce9f7d51a14ff702
 
 
 }
